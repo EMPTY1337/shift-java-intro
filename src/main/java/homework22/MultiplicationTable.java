019ba6124118333e36ec14bbcaa2cabf30a87862
Dmitry <rass219@mail.ru>
2025-11-16 03:43:28 +0700

    homework22 - реализована таблица умножения

---
+++ src/main/java/homework22/MultiplicationTable.java
@@ -0,0 +1,96 @@
+package main.java.homework22;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        // Создаем сканер для чтения ввода с клавиатуры
+        Scanner scanner = new Scanner(System.in);
+
+        // Запрашиваем у пользователя первое число
+        System.out.print("Введите первое число: ");
+        int firstNumber = scanner.nextInt(); // Читаем первое число
+
+        // Запрашиваем у пользователя второе число
+        System.out.print("Введите второе число: ");
+        int secondNumber = scanner.nextInt(); // Читаем второе число
+
+        // Запрашиваем шаг и проверяем что он положительный или не равен 0
+        int step;
+        do {
+            System.out.print("Введите шаг: ");
+            step = scanner.nextInt();
+            if (step <= 0) {
+                System.out.println("Шаг должен быть положительным или не 0.");
+            }
+        } while (step <= 0); // Повторяем пока шаг не станет положительным или больше 0
+
+        // Вызываем функцию для печати таблицы умножения
+        printMultiplicationTable(firstNumber, secondNumber, step);
+
+        scanner.close();
+    }
+
+    // Функция для вычисления ширины поля результата умножения
+    public static int getCellWidth(int number) {
+        String data = String.valueOf(number);
+        return data.length();
+    }
+
+    // Функция печатает таблицу умножения
+    public static void printMultiplicationTable(int firstNumber, int secondNumber, int step) {
+        // Находим меньшее число из двух введенных
+        int min = Math.min(firstNumber, secondNumber);
+        // Находим большее число из двух введенных
+        int max = Math.max(firstNumber, secondNumber);
+
+        // Вычисляем сколько чисел будет в таблице
+        int count = 0;
+        int current = min;
+        while (current <= max) {
+            count++;
+            current += step;
+        }
+
+        // Создаем массив для хранения всех чисел которые будут в таблице
+        int[] numbers = new int[count];
+        for (int i = 0; i < count; i++) {
+            numbers[i] = min + i * step;
+        }
+
+        // Находим самую широкую ячейку в таблице
+        int maxWidth = 0;
+        // Перебираем все возможные произведения чисел (все ячейки таблицы)
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < count; j++) {
+                int result = numbers[i] * numbers[j];
+                // Узнаем ширину этого числа (сколько символов занимает)
+                int width = getCellWidth(result);
+                // Если нашли число шире текущего максимума переписываем в maxWidth
+                if (width > maxWidth) {
+                    maxWidth = width;
+                }
+            }
+        }
+        // Добавляем пробелы чтобы числа визуально не касались друг друга)
+        maxWidth += 2;
+
+        // Печатаем первую строку таблицу
+        System.out.printf("%" + maxWidth + "s", "");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.printf("%" + maxWidth + "d", numbers[i]);
+        }
+        System.out.println();
+
+        // Печатаем саму таблицу умножения
+
+            for (int i = 0; i < count; i++) {
+            System.out.printf("%" + maxWidth + "d", numbers[i]);
+            for (int j = 0; j < count; j++) {
+                int result = numbers[i] * numbers[j];
+                System.out.printf("%" + maxWidth + "d", result);
+            }
+            System.out.println();
+        }
+    }
+}
